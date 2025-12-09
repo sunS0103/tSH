7cc6d8d3aa6c711f75510a5fe9983c0a84a86083
bhargav-b-7span <bhargav.b@7span.com>
2025-12-09 15:52:54 +0530

    Initial setup

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Header from "@/components/header";
+
+export default function Page() {
+  return (
+    <div className="container mx-auto h-screen">
+      <Header />
+    </div>
+  );
+}
---
+++ components/header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <div className="flex items-center justify-center h-full">
+        <img src="/Logo.svg" alt="logo" />
+    </div>
+  )
+}
