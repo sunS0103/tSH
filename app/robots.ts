0064774aa8980d40f35596cdd95fd815a739da22
harsh-t-7span <harsh.t@7span.com>
2026-01-01 12:57:33 +0530

    update: env support

--- app/robots.ts
+++ app/robots.ts
@@ -1,14 +1,14 @@
 import { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
-  const baseUrl = "https://techsmarthire.com";
+  const baseUrl = process.env.NEXT_PUBLIC_APP_URL as string;
 
   return {
     rules: {
       userAgent: "*",
       // Only allow indexing in production
-      allow: process.env.NODE_ENV === "production" ? "/" : "",
-      disallow: process.env.NODE_ENV === "production" ? "/private/" : "/",
+      allow: process.env.NEXT_PUBLIC_ENV === "production" ? "/" : "",
+      disallow: process.env.NEXT_PUBLIC_ENV === "production" ? "/private/" : "/",
     },
     sitemap: `${baseUrl}/sitemap.xml`,
   };
